ff5e3fffc2b81c5ee21e7ad688e3f5ef9557a706
ilikegoodfood <52924711+ilikegoodfood@users.noreply.github.com>
2019-08-27 17:55:50 +0100

    Removed Active Logging
    Removed Logging.

--- Verb Expansion Framework (1.0)/Source/Verb Expansion Framwork/Comps/VEF_ThingComp_HediffSet.cs
+++ Verb Expansion Framework (1.0)/Source/Verb Expansion Framwork/Comps/VEF_ThingComp_HediffSet.cs
@@ -36,45 +36,35 @@ namespace VerbExpansionFramework
 
         public void UpdateHediffSet()
         {
-            Log.Message("Updating Hediff Set");
             if (Pawn == null)
             {
-                Log.Message("Null Pawn");
                 return;
             }
             else if (Pawn.health.hediffSet.hediffs.NullOrEmpty() || this.hediffSetDef == null || this.hediffSetDef.hediffParts.NullOrEmpty())
             {
-                Log.Message("Null Hediffs");
                 Pawn.AllComps.Remove(this);
                 return;
             }
-            Log.Message("Passed Null Checks.");
 
             int count = 0;
             
             foreach (HediffDef hediffDef in this.hediffSetDef.hediffParts)
             {
-                Log.Message("Iterating for HediffParts");
                 if (Pawn.health.hediffSet.hediffs.FirstOrDefault(h => h.def == hediffDef) != null)
                 {
-                    Log.Message("HedifPart Found");
                     count += 1;
                 }
             }
-            Log.Message("Completed iterating for parts.");
 
             bool complete = count == hediffSetDef.hediffParts.Count;
-            Log.Message("Count = " + count + ", Complete = " + complete);
 
             HediffDef hediffDefIncomplete = hediffSetDef.setHediffIncomplete;
             HediffDef hediffDefComplete = hediffSetDef.setHediff;
             Hediff hediffIncomplete = Pawn.health.hediffSet.hediffs.FirstOrDefault(h => h.def == hediffDefIncomplete);
             Hediff hediffComplete = Pawn.health.hediffSet.hediffs.FirstOrDefault(h => h.def == hediffDefComplete);
-            Log.Message("Gathered Hediff Data and Hediffs");
 
             if (count == 0)
             {
-                Log.Message("Count = 0");
                 if (hediffIncomplete != null)
                 {
                     Pawn.health.RemoveHediff(hediffIncomplete);
@@ -87,7 +77,6 @@ namespace VerbExpansionFramework
             }
             else if (complete)
             {
-                Log.Message("Compete. Count = " + count);
                 if (hediffIncomplete != null)
                 {
                     Pawn.health.RemoveHediff(hediffIncomplete);
@@ -99,7 +88,6 @@ namespace VerbExpansionFramework
             }
             else
             {
-                Log.Message("Incomplete. Count =" + count);
                 if (hediffComplete != null)
                 {
                     Pawn.health.RemoveHediff(hediffComplete);
